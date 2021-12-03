e9d8da8ca2040d6399df195b5b1db23bef739191
jupiterorbita <38020888+jupiterorbita@users.noreply.github.com>
2021-12-02 22:34:54 -0700

    prepare w1d5 algos SLL printNth, reverse SLL

--- NinjaOOP/src/com/john/ninjas/Sensei.java
+++ NinjaOOP/src/com/john/ninjas/Sensei.java
@@ -1,8 +1,8 @@
 package com.john.ninjas;
 
-public class Sesnei extends SuperNinja implements Knowledgeable {
+public class Sensei extends SuperNinja implements Knowledgeable {
 
-	public Sesnei(String name, int powerLevel, String weapon) {
+	public Sensei(String name, int powerLevel, String weapon) {
 		super(name, powerLevel, weapon);
 	}
 	
--- NinjaOOP/src/com/john/ninjas/TestNinja.java
+++ NinjaOOP/src/com/john/ninjas/TestNinja.java
@@ -1,5 +1,7 @@
 package com.john.ninjas;
 
+import com.john.other.OtherClass;
+
 public class TestNinja {
 
 	public static void main(String[] args) {
@@ -13,6 +15,13 @@ public class TestNinja {
 		
 		goku.attack(kenshiro);
 		kenshiro.displayStats();
+		
+		OtherClass something = new OtherClass();
+		
+//		using the interface
+		Sensei masterSplinter = new Sensei("Master Splinter", 150, "cane");
+		String result = masterSplinter.sayWisdom();
+		System.out.println(masterSplinter.getName() + " from the " + masterSplinter.getClass().getSimpleName() + " Class, says " + result );
 
 	}
 
